e7a7452d02ff7321d7dc3ea127dee0222bcd39b8
bruderooo <szymonjacon@gmail.com>
2021-10-20 19:16:31 +0200

    Add function for noise generating

---
+++ utils.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
+
+from Discriminator import Discriminator
+from Generator import Generator
+
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
